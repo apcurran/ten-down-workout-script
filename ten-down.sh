8868d4ecd85c0f1e1b28b9a934c103ef25442b3e
Alex Curran <alexcurran.va@gmail.com>
2021-04-19 10:27:40 -0500

    Added workout

---
+++ ten-down.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/bash
+
+read -p "Hey Alex, how many sets did you do in your workout? " num_sets # variable created
+total_reps=0
+
+for ((i = 1; i <= num_sets; i++)); do
+    ((total_reps += i))
+done
+
+echo Total reps: $total_reps
