a516092983906e27d57e3c27e3fdf1c8610e92e7
Alex Curran <alexcurran.va@gmail.com>
2021-04-19 10:28:49 -0500

    Updated name

--- ten-down.sh
+++ ten-down.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/bash
 
-read -p "Hey Alex, how many sets did you do in your workout? " num_sets # variable created
+read -p "Hey user, how many sets did you do in your workout? " num_sets # variable created
 total_reps=0
 
 for ((i = 1; i <= num_sets; i++)); do
